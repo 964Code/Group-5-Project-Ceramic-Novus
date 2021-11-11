a2240b46d1372fcedbf9123c23e137fb8766561d
964Code <964code@gmail.com>
2021-11-11 11:46:59 +0100

    Added index.html, app.js, style.css. Contains responsive navbarand JS functionality.

---
+++ app.js
@@ -0,0 +1,9 @@
+// ------------------- NAVBAR JS -------------------------------------
+const toggleButton = document.querySelectorAll(".toggle-button")[0];
+const navbarLinks = document.querySelectorAll(".navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+// ----------------- NAVBAR JS END -----------------------------------
